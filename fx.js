834a05833bcb62924bfd4293f69d5ea980f0441b
Iwani Khalid <iwanikhalid@gmail.com>
2023-09-26 23:51:12 +0800

    alt fix #1

--- fx.js
+++ fx.js
@@ -1,5 +1,27 @@
+videoFilenamesCustom = '';
+videoFilenames = '';
+var hasBeenClicked = false;
+$('#thumbsCustom').hide();
+
 $(document).ready(function () {
-// dark mode switcher	 
+	
+	// CAN EDIT:: Insert numbers here for permanent listing
+	// Listing the filenames in array
+	var videoFilenames = ["1", "2"];
+	
+	// Admin Button
+	$('#admin').click(function(){
+		hasBeenClicked = true;	
+		load_custom();				
+	});
+	
+	if (hasBeenClicked) {		
+	} else {
+		//alert("prep to load default");
+		load_default();
+	}
+	
+	// Dark mode switcher	 
     $('.checkbox').change(function() {
         //if(this.checked) {
 			$('body').toggleClass("dark"); //you can list several class names 
@@ -7,50 +29,88 @@ $(document).ready(function () {
         //}         
     });
 	
+	// Load custom video list
+	function load_custom() {
+		alert("Hi parents! Welcome to LUMILA Admin. \n\nHere, you can change the no of videos for this current browsing session We don't store the data permanently.");
+		var adminpass = window.prompt("Enter password", "Enter password");
+		if(adminpass != 'durian') { 
+			$('#thumbsCustom').hide();
+			$('#thumbs').show();
+			alert("Wrong passowrd entered. Reverting to default no of videos"); 
+			//alert(videoFilenames);
+		}else{
+			$('#thumbsCustom').show();
+			$('#thumbs').hide();
+			// alert(videoFilenames);
+			var customVideoTotal = window.prompt("Enter new video total", "Use numbers only 1-100");
+			var customVideoTotal = parseInt(customVideoTotal); 
+			//alert(customVideoTotal);
+			// DONE: Generate videoFilenamesCustom array based on max no newvideocount
+			var min = 1, max = customVideoTotal;
+			var videoFilenamesCustom = Array.apply(null, {length: max + 1 - min}).map(function(_, idx) {
+				return idx + min;
+			});
+			//var videoFilenames = videoFilenamesCustom;
+			//alert(videoFilenames);
+			//console.log(x);
+			//alert(videoFilenamesCustom);
+			
+					videoFilenamesCustom = randomize(videoFilenamesCustom);
+					function randomize(arr) {
+						for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+						return arr;
+					}	
+					
+					$.each(videoFilenamesCustom, function( i, v ) {
+					  // alert(i + ": " + v );
+					  var i = Math.floor(Math.random() * videoFilenames.length);
+					  // i: position in array
+					  // v: value in that position
+					  
+					  $("#thumbsCustom").append('<div class=thumb genstyle><video controls preload=auto><source src=video/video_' + v + '.mp4#t=30.1 type=video/mp4></video></div>');
+
+					 $("video").on("play", function (e) {
+						//alert("playing");
+							$('video').not(this).each(function(){
+								var $playpause = $(this);
+								video = $playpause[0];
+								video.pause();
+							});
+					 });	  
+					  
+					});
+
+			}
+	}
 	
+	//  Load default video list
+	function load_default() {
+		// alert("ok");
+		videoFilenames = randomize(videoFilenames);
+		function randomize(arr) {
+			for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+			return arr;
+		}	
+		
+		$.each(videoFilenames, function( i, v ) {
+		  // alert(i + ": " + v );
+		  var i = Math.floor(Math.random() * videoFilenames.length);
+		  // i: position in array
+		  // v: value in that position
+		  
+			$("#thumbs").append('<div class=thumb genstyle><video controls preload=auto><source src=video/video_' + v + '.mp4#t=30.1 type=video/mp4></video></div>');
 
-	var videoFilenames = ["01", "02", "03", "04", "05", "06", "07"];
-	// Listing the filenames in array
-
-	var randomVideo = Math.floor(Math.random() * videoFilenames.length); 
-	// Selecting a random number (position in array) based on the number of filenames. It will start from 0
-
-	var videoFilename = videoFilenames[randomVideo];
-	// Get the filename belonging to the random position chosen above. 
-
-	//console.log(videoFilenames);
-	//console.log(randomVideo);
-	//console.log(videoFilename);
-	//$( "#thumbs" ).prepend('<div class=thumb genstyle><video controls><source src=video/video_' + videoFilename + '.mp4 type=video/mp4></video></div>');
-
-	// TODO: 
-	// DONE: Generate markup based on videoFilenames length eg: if 20, 20 video will be appended
-	// DONE: And each video is unique, no dupes
-	// DONE: Randomize the order of append/display. Cilok tapi tak faham haha!!
-
-	videoFilenames = randomize(videoFilenames);
-	function randomize(arr) {
-		for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
-		return arr;
+			 $("video").on("play", function (e) {
+				//alert("playing");
+					$('video').not(this).each(function(){
+						var $playpause = $(this);
+						video = $playpause[0];
+						video.pause();
+					});
+			 });	  
+		  
+		});
 	}	
 	
-	$.each(videoFilenames, function( i, v ) {
-	  // alert(i + ": " + v );
-	  var i = Math.floor(Math.random() * videoFilenames.length);
-	  // i: position in array
-	  // v: value in that position
-	  
-	  $( "#thumbs" ).prepend('<div class=thumb genstyle><video controls preload=auto><source src=video/video_' + v + '.mp4#t=10.1 type=video/mp4></video></div>');
-
-	 $("video").on("play", function (e) {
-		//alert("playing");
-			$('video').not(this).each(function(){
-				var $playpause = $(this);
-				video = $playpause[0];
-				video.pause();
-			});
-	 });	  
-	  
-	});
-
 }) 
+
